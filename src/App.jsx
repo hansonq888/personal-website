7ef187e05881856283284d928f3620246ba72769
Hanson Qin <hansonq888@gmail.com>
2025-09-22 00:20:38 -0400

    Finished loading screen, navbar next

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { LoadingScreen } from './components/LoadingScreen'
+import { Navbar } from "./components/Navbar";
 import "./index.css";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false)
   return (
     <>
-      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}{" "}
+        <div class={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}>
+          <h1>hanson qin</h1>
+          <Navbar />
+        </div>
     </>
   )
 }
